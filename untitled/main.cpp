da130fb234f317e72833839c79643b07f12d2f0b
phamminhkha102 <phamminhkha102@gmail.com>
2016-06-03 08:38:05 +0700

    thay doi i

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -9,11 +9,14 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     VideoCapture cap("Wildlife.wmv");
+    int i = 6;
+    i=7;
+    i=75;
     Mat mat;
     while(1){
         if(cap.read(mat))
             imshow("Opencv",mat);
+        waitKey(45);
     }
-
     return a.exec();
 }
